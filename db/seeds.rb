eb1248ffe0440826edbd8df8e90a77a30311341f
Aaron Anderson <aaron.russell.anderson@gmail.com>
2017-04-18 14:10:01 -0600

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -62,3 +62,12 @@ end
     )
 end
 puts "9 portfolio items added"
+
+3.times do |technology|
+	Portfolio.last.technologies.create!(
+		name: "Technology #{technology}"
+		portfolio_id: Portfolio.last.id	
+		)
+end
+puts "3 technologies added"
+
