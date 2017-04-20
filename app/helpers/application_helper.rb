d25d79ced7a77882c72ffa39491b0da49e0eaf93
Aaron Anderson <aaron.russell.anderson@gmail.com>
2017-04-20 15:39:59 -0600

    implemented the contact tag helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
        	 	(link_to "Registar",  new_user_registration_path) 
        	end
 	end
+
+	def source_helper(layout_name)
+		if session[:source]
+      		greeting = "Thanks for visiting me from #{session[:source]} you are using the #{layout_name} layout"
+      		content_tag(:p, greeting, class: "source-greeting")
+    	end
+    end
+
 end
